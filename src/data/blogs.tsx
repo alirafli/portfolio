6058d1153226582849df7bc8eb377babd7735768
alirafli <mrafli1212@gmail.com>
2024-03-25 00:10:52 +0700

    feat: move data

---
+++ src/data/blogs.tsx
@@ -0,0 +1,8 @@
+export const blogs = [
+	{
+		title:
+			'Implementing Jest-Image-Snapshot in a ReactJS Typescript Project with Puppeteer for Visual Slicing Comparison',
+		date: 'Mar 8, 2024',
+		href: 'https://medium.com/@mrafli1212/implementing-jest-image-snapshot-in-a-reactjs-typescript-project-with-puppeteer-for-visual-slicing-fd91b33fe5c4'
+	}
+]
---
+++ src/data/contactInfo.tsx
@@ -0,0 +1,31 @@
+export const contactInfo = [
+	{
+		title: 'Frontend Engineer',
+		icon: 'mdi:work-outline',
+		disabled: true
+	},
+	{
+		title: 'Resume',
+		icon: 'mdi:file-document-outline',
+		href: '/src/assets/CV_Mohammad Ali Rafli.pdf',
+		disabled: false
+	},
+	{
+		title: 'Linkedin',
+		icon: 'mdi:linkedin',
+		href: 'https://www.linkedin.com/in/mohammad-ali-rafli/',
+		disabled: false
+	},
+	{
+		title: 'mrafli1212@gmail.com',
+		icon: 'mdi:email-outline',
+		href: 'mailto:mrafli1212@gmail.com',
+		disabled: false
+	},
+	{
+		title: '@alirafli_',
+		icon: 'mdi:instagram',
+		href: 'https://www.instagram.com/alirafli_/',
+		disabled: false
+	}
+]
