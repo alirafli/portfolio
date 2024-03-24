0d887b3b0a53b929be31e11d2da51107e0078a62
alirafli <mrafli1212@gmail.com>
2024-03-25 00:50:57 +0700

    fix: add multiple to experiences

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -1,6 +1,7 @@
 export const experiences = [
 	{
 		title: 'Delos',
+		multiple: false,
 		role: 'Frontend Web Developer Intern',
 		date: 'Jun 2023 - Mar 2024',
 		imagePath: '/src/assets/delos-logo.png',
@@ -10,6 +11,7 @@ export const experiences = [
 	},
 	{
 		title: 'Ruangguru Bootcamp - Kampus Merdeka',
+		multiple: false,
 		role: 'Frontend Engineering',
 		date: 'Jan 2023 - Jul 2023',
 		imagePath: '/src/assets/ruangguru-logo.png',
@@ -19,6 +21,7 @@ export const experiences = [
 	},
 	{
 		title: 'Social Circles Co.',
+		multiple: false,
 		role: 'Software Engineer Intern',
 		date: 'Dec 2022 - Mar 2023',
 		imagePath: '/src/assets/socles-logo.jpg',
@@ -27,8 +30,8 @@ export const experiences = [
 		]
 	},
 	{
-		multiple: true,
 		title: 'Basic Computing Community (BCC)',
+		multiple: true,
 		imagePath: '/src/assets/bcc-logo.png',
 		multipleData: [
 			{
@@ -44,7 +47,7 @@ export const experiences = [
 				date: 'Jan 2022 - Dec 2022',
 				imagePath: '/src/assets/bcc-logo.png',
 				desc: [
-					'Oversaw and led multiple key projects, including software development and internal workshops, driving the enhancement of technical skills within the community.'
+					'Oversaw multiple key projects, including software development and internal workshops, driving the enhancement of technical skills within the community.'
 				]
 			},
 			{
@@ -58,23 +61,4 @@ export const experiences = [
 			}
 		]
 	}
-	// {
-	// 	title: 'Basic Computing Community (BCC)',
-	// 	role: 'Deputy Head of Frontend Department',
-	// 	date: 'Jan 2022 - Dec 2022',
-	// 	imagePath: '/src/assets/bcc-logo.png',
-	// 	desc: [
-	// 		'Oversaw and led multiple key projects, including software development and internal workshops, driving the enhancement of technical skills within the community.'
-	// 	]
-	// },
-	// {
-	// 	title: 'Basic Computing Community (BCC)',
-	// 	role: 'staff of Frontend Department',
-	// 	date: 'Mar 2021 - Jan 2022',
-	// 	imagePath: '/src/assets/bcc-logo.png',
-	// 	desc: [
-	// 		'Participating in an internship program provided at the early stage. Being part of a team with a product manager, product designer, and backend developer.',
-	// 		'Learning from basic JavaScript to mastering ReactJS. Attending workshops related to ReactJS covering topics from state management to testing.'
-	// 	]
-	// }
 ]
