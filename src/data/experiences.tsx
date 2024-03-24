1378d8ee2efd594bc31552532bc1353ba94dfe53
alirafli <mrafli1212@gmail.com>
2024-03-25 00:08:48 +0700

    feat: modify experiences

---
+++ src/data/experiences.tsx
@@ -0,0 +1,80 @@
+export const experiences = [
+	{
+		title: 'Delos',
+		role: 'Frontend Web Developer Intern',
+		date: 'Jun 2023 - Mar 2024',
+		imagePath: '/src/assets/delos-logo.png',
+		desc: [
+			'Enhancing and managing product line dashboards for efficient data management, utilizing Next.js and TypeScript for dashboard improvements and code optimization. Additionally, contributed to the design system by creating and refining components with TypeScript and Storybook, significantly improving interface consistency and usability across products.'
+		]
+	},
+	{
+		title: 'Ruangguru Bootcamp - Kampus Merdeka',
+		role: 'Frontend Engineering',
+		date: 'Jan 2023 - Jul 2023',
+		imagePath: '/src/assets/ruangguru-logo.png',
+		desc: [
+			"Learning web frontend skills through the MBKM program's Bootcamp Learning Plan, focusing on JavaScript and React.js in both basic and advanced front-end development."
+		]
+	},
+	{
+		title: 'Social Circles Co.',
+		role: 'Software Engineer Intern',
+		date: 'Dec 2022 - Mar 2023',
+		imagePath: '/src/assets/socles-logo.jpg',
+		desc: [
+			'Played a key role in the development of the Socles Social App, employing React Native Ignite (Typescript), and the Socles Web App, using Next.js (Typescript), contributing to significant user engagement growth.'
+		]
+	},
+	{
+		multiple: true,
+		title: 'Basic Computing Community (BCC)',
+		imagePath: '/src/assets/bcc-logo.png',
+		multipleData: [
+			{
+				role: 'Head of Frontend Department',
+				date: 'Dec 2022 - Oct 2023',
+				imagePath: '/src/assets/bcc-logo.png',
+				desc: [
+					'Oversaw and led multiple key projects, including software development and internal workshops, driving the enhancement of technical skills within the community.'
+				]
+			},
+			{
+				role: 'Deputy Head of Frontend Department',
+				date: 'Jan 2022 - Dec 2022',
+				imagePath: '/src/assets/bcc-logo.png',
+				desc: [
+					'Oversaw and led multiple key projects, including software development and internal workshops, driving the enhancement of technical skills within the community.'
+				]
+			},
+			{
+				role: 'staff of Frontend Department',
+				date: 'Mar 2021 - Jan 2022',
+				imagePath: '/src/assets/bcc-logo.png',
+				desc: [
+					'Participating in an internship program provided at the early stage. Being part of a team with a product manager, product designer, and backend developer.',
+					'Learning from basic JavaScript to mastering ReactJS. Attending workshops related to ReactJS covering topics from state management to testing.'
+				]
+			}
+		]
+	}
+	// {
+	// 	title: 'Basic Computing Community (BCC)',
+	// 	role: 'Deputy Head of Frontend Department',
+	// 	date: 'Jan 2022 - Dec 2022',
+	// 	imagePath: '/src/assets/bcc-logo.png',
+	// 	desc: [
+	// 		'Oversaw and led multiple key projects, including software development and internal workshops, driving the enhancement of technical skills within the community.'
+	// 	]
+	// },
+	// {
+	// 	title: 'Basic Computing Community (BCC)',
+	// 	role: 'staff of Frontend Department',
+	// 	date: 'Mar 2021 - Jan 2022',
+	// 	imagePath: '/src/assets/bcc-logo.png',
+	// 	desc: [
+	// 		'Participating in an internship program provided at the early stage. Being part of a team with a product manager, product designer, and backend developer.',
+	// 		'Learning from basic JavaScript to mastering ReactJS. Attending workshops related to ReactJS covering topics from state management to testing.'
+	// 	]
+	// }
+]
