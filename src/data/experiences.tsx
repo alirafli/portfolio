cc954f6f4f2bd0757f20049f91c33b5bbcf8d2ad
alirafli <mrafli1212@gmail.com>
2024-03-26 21:42:57 +0700

    feat: add tech stack to ruangguru bootcamp

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -16,6 +16,7 @@ export const experiences = [
 		role: 'Frontend Engineering',
 		date: 'Jan 2023 - Jul 2023',
 		imagePath: '/src/assets/ruangguru-logo.png',
+		techStack: ['General Competence', 'Software Engineering', 'Javascript', 'ReacjtJS'],
 		desc: [
 			"Learning web frontend skills through the MBKM program's Bootcamp Learning Plan, focusing on JavaScript and React.js in both basic and advanced front-end development."
 		]
@@ -26,7 +27,7 @@ export const experiences = [
 		role: 'Software Engineer Intern',
 		date: 'Dec 2022 - Mar 2023',
 		imagePath: '/src/assets/socles-logo.jpg',
-		techStack: ['NextJS', 'React Native - Ignite', 'Typescript', 'SWR','MobX', 'Tailwind'],
+		techStack: ['NextJS', 'React Native - Ignite', 'Typescript', 'SWR', 'MobX', 'Tailwind'],
 		desc: [
 			'Played a key role in the development of the Socles Social App, employing React Native Ignite (Typescript), and the Socles Web App, using Next.js (Typescript), contributing to significant user engagement growth.'
 		]
